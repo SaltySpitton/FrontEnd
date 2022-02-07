a88e054a21c4f98fc663824413a32f9c6600029b
andrewurq <andrewurq@gmail.com>
2022-02-07 14:42:54 -0500

    :tada: Add: homepage images, button, social links, working on forms

---
+++ src/components/Home.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import '../css/stackdev.css'
+import logo from '../images/logo.svg'
+import reactIcon from '../images/react-original-wordmark.svg'
+import htmlIcon from '../images/html5-original-wordmark.svg'
+import cssIcon from '../images/css3-original-wordmark.svg'
+import javaIcon from '../images/javascript-original.svg'
+import djangoIcon from '../images/django-original.svg'
+import cplusIcon from '../images/cplusplus-original.svg'
+import qaBubbles from '../images/QA-bubbles.svg'
+import checkMark from "../images/Vector 1.png"
+import upvoted from '../images/votes-up.svg'
+
+
+const Home = () => {
+  return(<>
+
+    <div className="formSpace">
+      <div className="formSpaceLeft">
+        <h1>
+          <img className="mainlogo" 
+          src={logo} alt="Stack dev logo" /> 
+        </h1>
+
+        <h1> 
+            Q&A Without Distractions
+        </h1>
+        
+        <p
+          >With your help, we’re working together to build a library of detailed answers to every question about programming. <strong>It’s built and run by you.</strong>
+        </p>
+
+        <button id="exploreBttn" 
+          className="standardBttn">
+          Explore Questions
+        </button>
+      </div>
+      
+      <div className="formSpaceRight">
+        <p> <a href="">Log in</a> </p>
+
+         
+      </div>
+    </div>
+   
+
+    <div className="homeIcons">
+      <img className='homeIcon' src={reactIcon} alt="React Icon" />
+      <img className='homeIcon' src={htmlIcon} alt="HTML Icon" />
+      <img className='homeIcon' src={cssIcon} alt="CSS Icon" />
+      <img className='homeIcon' src={javaIcon} alt="Java Icon" />
+      <img className='homeIcon' src={djangoIcon} alt="Django Icon" />
+      <img className='homeIcon' src={cplusIcon} alt="C++ Icon" />
+    </div>
+   <div className="homeQuestions">
+      <div className="homeLeft">
+        <h1> 
+          Ask Questions, Get Answers
+        </h1>
+        <p>
+          This site is all about getting answers. It’s not a discussion forum. There’s  no chit-chat
+        </p>
+        <br /><br />
+
+        <h3>
+          <img className='listIcon' src={upvoted} alt="up voted image" />
+          Good answers are voted up and rise to the top
+        </h3>
+        
+        <p>
+          The best answers show up first so that they are always easy to find
+        </p>
+        <br />
+
+        <h3>
+          <img  className='listIcon' src={checkMark} alt="Checkmark" />
+          The person who asked can mark one answer as “accepted”
+        </h3>
+        <p>
+          Accepting the best answer just means that it worked for the person who asked
+        </p>
+      </div>
+      <div className="homeRight">
+        <img className="homeRight" src={qaBubbles} alt="Q&A bubble picture" />
+
+      </div>
+      
+    </div>
+    
+    
+    <br />
+
+  </>)
+}
+
+export default Home;
---
+++ src/components/Layout.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import footLogo from '../images/foot-t-logo.png'
+
+const Layout = ({children}) => {
+
+  return( <> 
+
+        { children }
+
+        <footer>
+           <table  className="footerTable">
+               <thead>
+                   <th><img src={footLogo} alt="Stack Dev Logo" />
+                        StackDevHelp
+                    </th>
+                    
+                   <th>Resources</th>                 
+                   <th>Creators</th>
+                   <tr></tr><tr></tr>
+                   
+               </thead>
+               <tbody>
+                   <tr>
+                        <td>Questions</td>
+                        
+                        <td> <a href="https://developer.mozilla.org/en-US/"        target="_blank"> 
+                            MDN</a>
+                        </td>
+
+                        <td> <a href="www.andrewurquhart.com" target="_blank">
+                            Andrew</a>
+                        </td>
+                    </tr>
+                   
+                   <tr>
+                        <td> <a href="https://www.linkedin.com/jobs/search/?keywords=software%20engineer" target="_blank"> 
+                            Jobs</a>
+                        </td>
+
+                        <td> <a href="https://www.w3schools.com/" target="_blank"> 
+                            W3 Schools</a>                        
+                        </td>
+
+                        <td>Ali</td>
+                   </tr>
+                   <tr>
+                        <td>User Directory</td>
+
+                        <td> <a href="https://medium.com/" target="_blank">
+                            Medium</a>
+                        </td>
+
+                        <td>Daniele</td>
+                   </tr>
+                   <tr>
+                        <td>Open Source</td>
+                        
+                        <td> <a href="https://thehackernews.com/" target="_blank"> 
+                            Hacker News</a>
+                        </td>
+
+                        <td>Sylvie</td>
+                   </tr>
+               </tbody>
+           </table>
+        </footer>
+    </>)
+}
+
+export default Layout;
