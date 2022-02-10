5f9c4e0c84375d19d68650eb96e769977ef6e1f5
Andrew D Urquhart <85643280+BostonTechie@users.noreply.github.com>
2022-02-10 15:23:14 -0500

    :tada: added in styling for user dashboard

--- src/components/UserDataProfile.js
+++ src/components/UserDataProfile.js
@@ -1,73 +1,97 @@
-import React from 'react'
-import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
+import * as React from 'react';
 import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 
 const UserDataProfile = () => {
   return (
     <div>
-    <Stack sx={{
-           height: "50%"}}
-      direction={{ xs: 'column', sm: 'column' }}
-      spacing={{  xs: 2, sm: 2, md: 4 }}>
-      
-      <h2>About</h2>
-      <Item sx={{width: '100%',
-           
-            border: 2,
-            borderRadius: 2,
-            boxShadow: 3,}}>
-            About
-        </Item>
     
-     
-        <Stack direction={{ xs: 'column', sm: 'row' }}
-            spacing={{  xs: 2, sm: 2, md: 4 }}>
-            <Item sx={{minWidth: '50%',
-                border: 2,
-                borderRadius: 2,
-                boxShadow: 3,}}>
-                Answers
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={4}>
+         <Grid item xs={3}>
+                Image
+                <Item sx={{
+                    border: 2,
+                    borderRadius: 2,
+                    boxShadow: 3, }}>
+                    Image-content
+                </Item>
+          </Grid>
+
+            <Grid item xs={9}>
+            User Data
+            <Item  sx={{
+                    border: 2,
+                    borderRadius: 2,
+                    boxShadow: 0, }}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt corporis repellat architecto dolorem porro maiores provident quam quae consectetur voluptatem. Nemo dolores mollitia impedit ad iusto id tenetur. Praesentium, explicabo!
+                </Item>
+            </Grid>
+
+            <Grid item xs={16}  >
+            About
+            <Item sx={{
+                    border: 2,
+                    borderRadius: 2,
+                    boxShadow: 3, }}>
+                    about-content
             </Item>
+            </Grid>
 
-            <Item sx={{minWidth: '50%',
-                border: 2,
-                borderRadius: 2,
-                boxShadow: 3,}}>
-                Questions
-            </Item>
+            <Grid item xs={16} lg={6}>
+            Answers
+            <Item sx={{
+                    border: 2,
+                    borderRadius: 2,
+                    boxShadow: 3, }}>
+                    answers-content
+                </Item>
+            </Grid>
 
-        </Stack>
+            <Grid item xs={16} lg={6}>
+            Questions
+            <Item sx={{
+                    border: 2,
+                    borderRadius: 2,
+                    boxShadow: 3, }}>
+                    questions-content
+                </Item>
+            </Grid>
 
-        <Stack direction={{ xs: 'column', sm: 'row' }}
-            spacing={{  xs: 1, sm: 2, md: 4 }}>
-            <Item sx={{minWidth: '50%',
-                maxWidth:'50%',
-                border: 2,
-                borderRadius: 2,
-                boxShadow: 3,}}>
-                Votes
-            </Item>
+            <Grid item xs={16} lg={6}>
+            Votes
+            <Item sx={{
+                    border: 2,
+                    borderRadius: 2,
+                    boxShadow: 3, }}>
+                    votes-content
+                </Item>
+            </Grid>
+            
+            <Grid item xs={16} lg={6}>
+            Top tags
+            <Item sx={{
+                    border: 2,
+                    borderRadius: 2,
+                    boxShadow: 3, }}>
+                    Top tags-content
+                </Item>
+            </Grid>
+      </Grid>
+    </Box>
 
-            <Item sx={{minWidth: '50%',
-                maxWidth:'50%',
-                border: 2,
-                borderRadius: 2,
-                boxShadow: 3,}}>
-                Top Tags
-            </Item>
 
-        </Stack>
-        
-    </Stack>
+
   </div>
   )
 }
