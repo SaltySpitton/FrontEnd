14d9778573571b419a42718e1d7388964dbf53be
alialhakeem <ali.b.alhakim@gmail.com>
2022-02-09 23:05:22 -0700

    :tada: create user login and register form with user authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,87 @@
-import React from 'react';
+import React, { useState } from "react";
+
+import Axios from "axios";
 
 const Login = () => {
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [data, setData] = useState(null);
+  const register = () => {
+    Axios({
+      method: "POST",
+      data: {
+        username: registerUsername,
+        password: registerPassword,
+        email: registerEmail,
+      },
+      withCredentials: true,
+      url: "http://localhost:4200/users/register",
+    }).then((res) => console.log(res));
+  };
+  const login = () => {
+    Axios({
+      method: "POST",
+      data: {
+        username: loginUsername,
+        password: loginPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:4200/users/login",
+    }).then((res) => console.log(res));
+  };
+  const getUser = () => {
+    Axios({
+      method: "GET",
+      withCredentials: true,
+      url: "http://localhost:4200/users",
+    }).then((res) => {
+      setData(res.data);
+      console.log(res.data);
+      console.log(`we hit this routr`);
+    });
+  };
+  return (
+    <div className="App">
+      <div>
+        <h1>Register</h1>
+        <input
+          placeholder="username"
+          onChange={(e) => setRegisterUsername(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          onChange={(e) => setRegisterPassword(e.target.value)}
+        />
+        <input
+          placeholder="email"
+          onChange={(e) => setRegisterEmail(e.target.value)}
+        />
+        <button onClick={register}>Submit</button>
+      </div>
 
-    
-  return( <>
-  
-    <p>Login</p>
-  </>)
+      <div>
+        <h1>Login</h1>
+        <input
+          placeholder="username"
+          onChange={(e) => setLoginUsername(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          onChange={(e) => setLoginPassword(e.target.value)}
+        />
+        <button onClick={login}>Submit</button>
+      </div>
+
+      <div>
+        <h1>Get User</h1>
+        <button onClick={getUser}>Submit</button>
+        {data ? <h1>Welcome Back {data.username}</h1> : null}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
