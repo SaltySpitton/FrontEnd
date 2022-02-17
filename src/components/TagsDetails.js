d8948202c086f0811447860f0e0398ab5b3864d2
Andrew D Urquhart <85643280+BostonTechie@users.noreply.github.com>
2022-02-17 14:57:19 -0500

    all done

--- src/components/TagsDetails.js
+++ src/components/TagsDetails.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
-import { Box, Paper, Grid, Container, CardMedia, Typography, IconButton, Button } from '@mui/material';
+import { Box, Paper, Grid, Container, CardMedia, Typography, IconButton, Button, TextField, InputLabel } from '@mui/material';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EditIcon from '@mui/icons-material/Edit';
-import { useContext, useEffect } from "react";
-import UserContext from './UserContext'
 import Axios from "axios";
+import { useState, useContext,  useEffect } from "react";
+import arrayofTags from './arrayofTags/arrayofTags';
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -20,53 +20,46 @@ const Item = styled(Paper)(({ theme }) => ({
 
 
 
+
 const TagsDetails = () => {
+  
 
-  const tagsArray = [
-    {name: "C#",
-     descrip: "C# (pronounced \"see sharp\") is a high level, statically typed, multi-paradigm programming language developed by Microsoft. C# code usually targets Microsoft's .NET family of tools and run-times, which include .NET, .NET Framework and Xamarin among others. Use this tag for questions about code written in C# or about C#'s formal specification." },{
-    
-      name: "CX",
-      descrip: "C;ddsflon." },{
-      
-      name: "fff",
-      descrip: "C;ddsflon." 
-
-      }
-    
-    
-    
-    ]
-
-    console.log(tagsArray)
+  const [ backgroundColor, setbackgroundColor ] = useState('#d0e6ba')
+  const [ display, setDisplay ] = useState('inline-block')
   return (
     <>
-      <Container>
       <Box sx={{ flexGrow: 1 }}>
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-      >
-        {Array.from(Array(tagsArray)).map((tag, index) => (
-          
-          <Grid item xs={12} sm={12} md={6} key={index}>
-            <Item key={index} sx={{boxShadow: 0 }}>
-              {tag[index].name}
-            </Item>
-            <Item key={index} sx={{boxShadow: 0 }}>
-              {tag[index].descrip}
-            </Item>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {arrayofTags.map(({ nameTag, descripTag }) =>  (
+              <Grid 
+                item xs={12} sm={12} md={4} key={nameTag}>
+                <Box sx={{
+                  border: 2,
+                }}>
+
+                <Item item='auto' sx={{
+                boxShadow: 0,
+                textAlign: 'left',}}>  
+                <div style={{  'backgroundColor': backgroundColor, 'display' : display }}  >
+                   {nameTag}
+                </div>
+                </Item>
+
+                <Item sx={{
+                boxShadow: 0,
+                textAlign: 'left' }}>
+                  {descripTag}
+                </Item>
+                
+                </Box>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-    </Box>
-    </Container>
-      
-      
-     
+        </Box>
     </>
   )
 }
+
+
 
 export default TagsDetails
